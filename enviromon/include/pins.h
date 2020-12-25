5de39aaef831f2c283e5e4375fd33267c2c49029
Alexander Hemberg <alexander.hemberg@gmail.com>
2020-12-25 17:55:57 +0100

    Failed to add submodule

--- enviromon/include/pins.h
+++ enviromon/include/pins.h
@@ -1,1 +1,2 @@
 #define CLI_ENABLE 8
+#define ONE_WIRE_BUS 6
--- enviromon/src/main.cpp
+++ enviromon/src/main.cpp
@@ -2,17 +2,27 @@
 
 #include <Arduino.h>
 #include <Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <DS3231.h>
 #include <cli.h>
 
 DS3231 Clock;
 Cli cli = Cli(Clock);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature tempSensors(&oneWire);
 
+
+float getTemperature() {
+  tempSensors.requestTemperatures();
+  return tempSensors.getTempCByIndex(0);
+}
 
 void setup()
 {
   Serial.begin(9600);
   Wire.begin();
+  tempSensors.begin();
   pinMode(CLI_ENABLE, INPUT_PULLUP);
 }
 
@@ -21,4 +31,9 @@ void loop()
   if (digitalRead(CLI_ENABLE) == LOW) {
     cli.mainL();
   }
+  
+  Serial.print(getTemperature());
+  Serial.println("C");
+
+  
 }
